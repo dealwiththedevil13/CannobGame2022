7f10afc399c89b4c66502fe5e82326f21dd91e83
Amanda <10932525@uvu.edu>
2022-10-14 22:38:47 -0600

    Vector3Data, Vector3DataList, and Instancer

--- CannonG/Assets/Scripts/Instancer.cs
+++ CannonG/Assets/Scripts/Instancer.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Instancer : MonoBehaviour
+[CreateAssetMenu]
+
+public class Instancer : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject prefab;
+    private int num;
+
+    public void CreateInstance()
     {
-        
+        Instantiate(prefab);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateInstance(Vector3Data obj)
     {
-        
+        Instantiate(prefab, obj.value, Quaternion.identity);
+    }
+
+    public void CreateInstanceFromList(Vector3DataList obj)
+    {
+        foreach (var t in obj.vector3DList)
+        {
+            Instantiate(prefab, t.value, Quaternion.identity);
+        }
+    }
+
+
+    public void CreateInstanceFromListCounting(Vector3DataList obj)
+    {
+        Instantiate(prefab, obj.vector3DList[num].value, Quaternion.identity);
+        num++;
+        if(num == obj.vector3DList.Count)
+        {
+            num =0;
+        }
+    }
+
+    public void CreateInstanceFromListRandomly(Vector3DataList obj)
+    {
+        num = Random.Range(0, obj.vector3DList.Count - 1);
+        Instantiate(prefab, obj.vector3DList[num].value, Quaternion.identity);
     }
 }
