75dafcd3fdc20c2e1bda3cdd34744897743ad589
Amanda <10932525@uvu.edu>
2022-10-31 01:02:02 -0600

    Ring and dot placeholders

--- CannonG/Assets/Scripts/DraggableBehavior.cs
+++ CannonG/Assets/Scripts/DraggableBehavior.cs
@@ -1,18 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DraggableBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Camera cameraObj;
+    public bool draggable;
+    public Vector3 position, offset;
+    public UnityEvent startDragEvent, endDragEvent;
     void Start()
     {
-        
+        cameraObj = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator OnMouseDown()
     {
-        
+        offset = transform.position -cameraObj.ScreenToWorldPoint(Input.mousePosition);
+        draggable = true;
+        startDragEvent.Invoke();
+        yield return new WaitForFixedUpdate();
+
+        while (draggable)
+        {
+            yield return new WaitForFixedUpdate();
+            position = cameraObj.ScreenToWorldPoint(Input.mousePosition) + offset;
+            transform.position = position;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        draggable = false;
+        endDragEvent.Invoke();
     }
 }
