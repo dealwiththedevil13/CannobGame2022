31684aaa0475c9b0b0314f5f658302be1e15b6de
Amanda <10932525@uvu.edu>
2022-12-10 04:42:32 -0700

    Destruction on collision with enemy

--- CannonG/Assets/Scripts/CollisionBehavior.cs
+++ CannonG/Assets/Scripts/CollisionBehavior.cs
@@ -1,11 +1,16 @@
 using System;
 using UnityEngine;
-
+//Script by Education Public https://www.youtube.com/watch?v=5AsON1__quw&list=PL-ptF2slHtJBPrCYJRkATzjyxmcUuIL5D&index=8
 public class CollisionBehavior : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.collider.tag == "Enemy");
-        
+        if (other.tag == "")
+        {
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+
     }
 }
