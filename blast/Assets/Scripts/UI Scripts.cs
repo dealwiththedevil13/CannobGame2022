bcb2afb755838ce545505016142f0146f153aeeb
Amanda <10932525@uvu.edu>
2023-07-27 23:47:48 -0600

    edited my UI script

--- blast/Assets/Scripts/UI Scripts.cs
+++ blast/Assets/Scripts/UI Scripts.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class UIScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Use this as a base of opperations
+    public void Menu()
     {
         
     }
